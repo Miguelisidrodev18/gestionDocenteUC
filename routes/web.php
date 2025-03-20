4f8217175ad0f6b1fa69740a00f499fc9372e3b7
SEMANA4 <74093841@continental.edu.pe>
2025-03-20 17:40:34 -0500

    Cambios en Docente

--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Http\Controllers\DocenteController;
 use Illuminate\Support\Facades\Route;
 use Inertia\Inertia;
 
@@ -11,5 +12,13 @@ Route::get('dashboard', function () {
     return Inertia::render('Dashboard');
 })->middleware(['auth', 'verified'])->name('dashboard');
 
+Route::middleware(['auth', 'verified'])->group(function () {
+    Route::get('/docentes',[DocenteController::class,'index'])->name('docentes.index');
+    Route::get('/docentes/create',[DocenteController::class,'create'])->name('docentes.create');
+    Route::post('/docentes',[DocenteController::class,'store'])->name('docentes.store');
+    Route::get('/docentes/{docente}/edit',[DocenteController::class,'edit'])->name('docentes.edit');
+    Route::put('/docentes/{docente}',[DocenteController::class,'update'])->name('docentes.update');
+    Route::delete('/docentes/{docente}',[DocenteController::class,'destroy'])->name('docentes.destroy');
+});
 require __DIR__.'/settings.php';
 require __DIR__.'/auth.php';
