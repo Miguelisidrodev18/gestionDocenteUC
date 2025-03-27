bfdde833a2b571bd4175f851a9360428099193cc
SEMANA4 <74093841@continental.edu.pe>
2025-03-27 16:13:27 -0500

    rutas agregadas en web.php

--- routes/web.php
+++ routes/web.php
@@ -20,5 +20,39 @@ Route::middleware(['auth', 'verified'])->group(function () {
     Route::put('/docentes/{docente}',[DocenteController::class,'update'])->name('docentes.update');
     Route::delete('/docentes/{docente}',[DocenteController::class,'destroy'])->name('docentes.destroy');
 });
+
+Route::middleware(['auth', 'verified'])->group(function () {
+    Route::get('/cursos',[CursoController::class,'index'])->name('cursos.index');
+    Route::get('/cursos/create',[CursoController::class,'create'])->name('cursos.create');
+    Route::post('/cursos',[CursoController::class,'store'])->name('cursos.store');
+    Route::get('/cursos/{curso}/edit',[CursoController::class,'edit'])->name('cursos.edit');
+    Route::put('/cursos/{curso}',[CursoController::class,'update'])->name('cursos.update');
+    Route::delete('/cursos/{curso}',[CursoController::class,'destroy'])->name('cursos.destroy');
+});
+Route::middleware(['auth', 'verified'])->group(function () {
+    Route::get('/horarios',[HorarioController::class,'index'])->name('horarios.index');
+    Route::get('/horarios/create',[HorarioController::class,'create'])->name('horarios.create');
+    Route::post('/horarios',[HorarioController::class,'store'])->name('horarios.store');
+    Route::get('/horarios/{horario}/edit',[HorarioController::class,'edit'])->name('horarios.edit');
+    Route::put('/horarios/{horario}',[HorarioController::class,'update'])->name('horarios.update');
+    Route::delete('/horarios/{horario}',[HorarioController::class,'destroy'])->name('horarios.destroy');
+});
+
+Route::middleware(['auth', 'verified'])->group(function () {
+    Route::get('/actualizaciones',[ActualizacionController::class,'index'])->name('actualizaciones.index');
+    Route::get('/actualizaciones/create',[ActualizacionController::class,'create'])->name('actualizaciones.create');
+    Route::post('/actualizaciones',[ActualizacionController::class,'store'])->name('actualizaciones.store');
+    Route::get('/actualizaciones/{actualizacion}/edit',[ActualizacionController::class,'edit'])->name('actualizaciones.edit');
+    Route::put('/actualizaciones/{actualizacion}',[ActualizacionController::class,'update'])->name('actualizaciones.update');
+    Route::delete('/actualizaciones/{actualizacion}',[ActualizacionController::class,'destroy'])->name('actualizaciones.destroy');
+});
+Route::middleware(['auth','verified'])->group(function () {
+    Route::get('/materiales', [MaterialController::class,'index'])->name('materiales.index');  
+    Route::get('/materiales/create',[MaterialController::class,'create'])->name('materiales.create');
+    Route::post('/materiales',[MaterialController::class,'store'])->name('materiales.store');
+    Route::get('/materiales/{material}/edit',[MaterialController::class,'edit'])->name('materiales.edit');
+    Route::put('/materiales/{material}',[MaterialController::class,'update'])->name('materiales.update');
+    Route::delete('/materiales/{material}',[MaterialController::class,'destroy'])->name('materiales.destroy'); 
+});
 require __DIR__.'/settings.php';
 require __DIR__.'/auth.php';
